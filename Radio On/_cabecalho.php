d95a2ef773613e121834ad4099862b1d7335f4eb
guifereira2001 <37638123+guifereira2001@users.noreply.github.com>
2018-03-21 21:18:46 +0000

    Add files via upload

---
+++ Radio On/_cabecalho.php
@@ -0,0 +1,20 @@
+<?php
+//=======================================================
+// cabecalho
+//=======================================================
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rádio Music ON</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css     ">
+    <link rel="stylesheet" href="mainsa.css">
+</head>
+<body>
---
+++ Radio On/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//=======================================================
+// index
+//=======================================================
+     
+include_once('_cabecalho.php');
+
+echo '<h2 class="test-center">Bem Vindo a Radio ON!</h2';   
+
+include_once('_rodape.php');
+
+?>
